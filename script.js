39223c9a3f1e0458a164237cd4d2e2b870fbc9ca
ero-sannin2 <rider.dragon320@gmail.com>
2025-06-08 19:48:34 +0545

    add num button event listener

---
+++ script.js
@@ -0,0 +1,61 @@
+let operand1="";
+let operand2="";
+let operator="";
+
+
+let output=document.querySelector(".output");
+let delButton=document.querySelector("#del");
+let acButton=document.querySelector("#ac");
+let numButtons=document.querySelectorAll("#calc-button-num");
+let opButtons=document.querySelectorAll("#calc-button-op");
+let calculateButton=document.querySelector("#calc-button-equal");
+
+
+
+function operate(){
+    if(operator=='+'){
+        return operand1+operand2;
+    }
+    else if(operator=='-'){
+        return operand1-operand2;
+    }
+    else if(operator=='*'){
+        return operand1*operand2;
+    }
+    else{
+        return operand1/operand2;
+    }
+}
+
+function add(){
+    if(output.id==""){
+        operand1=output.textContent;
+        output.id="p"
+    }
+}
+
+
+numButtons.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        if(output.id==""||output.id=="result"){
+            output.textContent="";
+            output.textContent+=e.target.textContent;
+            operand1+=e.target.textContent;
+            output.id="firstop";
+        }
+        else if(output.id=="secondop"){
+            output.textContent+=e.target.textContent;
+            operand2+=e.target.textContent;
+        }
+        else if(output.id=="firstop"){
+            output.textContent+=e.target.textContent;
+            operand1+=e.target.textContent;
+        }
+    })
+});
+
+opButtons.forEach((element)=>{
+    element.addEventListener("click",(e)=>{
+        
+    })
+})
