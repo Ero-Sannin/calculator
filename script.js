96bad8ab6548ccd64156456b3a9e597e5b60a819
ero-sannin2 <rider.dragon320@gmail.com>
2025-06-10 18:07:28 +0545

    fix op button event listener

--- script.js
+++ script.js
@@ -1,78 +1,94 @@
-let operand1="";
-let operand2="";
-let operator="";
+let operand1 = "";
+let operand2 = "";
+let operator = "";
 
 
-let output=document.querySelector(".output");
-let delButton=document.querySelector("#del");
-let acButton=document.querySelector("#ac");
-let numButtons=document.querySelectorAll("#calc-button-num");
-let opButtons=document.querySelectorAll("#calc-button-op");
-let calculateButton=document.querySelector("#calc-button-equal");
+let output = document.querySelector(".output");
+let delButton = document.querySelector("#del");
+let acButton = document.querySelector("#ac");
+let numButtons = document.querySelectorAll("#calc-button-num");
+let opButtons = document.querySelectorAll("#calc-button-op");
+let calculateButton = document.querySelector("#calc-button-equal");
 
 
-function clear(){
-    output.textContent="";
-    output.id="";
+function clear() {
+    output.textContent = "";
+    output.id = "";
 }
-function operate(){
-    if(operator=='+'){
-        return operand1+operand2;
+function operate() {
+    if (operator == '+') {
+        return operand1 + operand2;
     }
-    else if(operator=='-'){
-        return operand1-operand2;
+    else if (operator == '-') {
+        return operand1 - operand2;
     }
-    else if(operator=='*'){
-        return operand1*operand2;
+    else if (operator == 'X') {
+        return operand1 * operand2;
     }
-    else{
-        return operand1/operand2;
+    else {
+        return operand1 / operand2;
     }
 }
 
-function add(){
-    if(output.id==""){
-        operand1=output.textContent;
-        output.id="p"
+function add() {
+    if (output.id == "") {
+        operand1 = output.textContent;
+        output.id = "p"
     }
 }
 
 
 numButtons.forEach(element => {
-    element.addEventListener("click",(e)=>{
-        if(output.id==""||output.id=="result"){
-            output.textContent="";
-            output.textContent+=e.target.textContent;
-            operand1+=e.target.textContent;
-            output.id="firstop";
+    element.addEventListener("click", (e) => {
+        if (output.id == "" || output.id == "result") {
+            output.textContent = "";
+            output.textContent += e.target.textContent;
+            operand1 += e.target.textContent;
+            output.id = "firstop";
         }
-        else if(output.id=="secondop"){
-            output.textContent+=e.target.textContent;
-            operand2+=e.target.textContent;
+        else if (output.id == "secondop") {
+            output.textContent += e.target.textContent;
+            operand2 += e.target.textContent;
         }
-        else if(output.id=="firstop"){
-            output.textContent+=e.target.textContent;
-            operand1+=e.target.textContent;
+        else if (output.id == "firstop") {
+            output.textContent += e.target.textContent;
+            operand1 += e.target.textContent;
         }
     })
 });
 
-opButtons.forEach((element)=>{
-    element.addEventListener("click",(e)=>{
-        if(output.id=="firstop"){
-            output.textContent="";
-            operator=e.target.textContent;
+opButtons.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        if (output.id == "firstop") {
+            operand1 = output.textContent;
+            output.textContent = "";
+            operator = e.target.textContent;
+            output.id = "secondop";
         }
-        else if(output.id=="secondop"){
-            operate();
-            operand1=output.textContent;
-            operator=e.target.textContent;
+        else if (output.id == "secondop") {
+
+            if (output.textContent != "") {
+                operand2 = output.textContent;
+                operate();
+                output.id = "secondop";
+                operand1 = output.textContent;
+                operator = e.target.textContent;
+                output.textContent = "";
+            }
+            else {
+                operator = e.target.textContent;
+            }
+
+
         }
-        else if(output.id=="result"){
-            operator=e.target.textContent;
-            operand1=output.textContent;
+        else if (output.id == "result") {
+            operator = e.target.textContent;
+            operand1 = output.textContent;
+            output.id = "secondop";
         }
     })
 })
 
-acButton.addEventListener("click",clear);
+acButton.addEventListener("click", clear);
+
+
